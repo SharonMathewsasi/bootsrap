99e70f0667996b99f518c2317b2928fbb6c7b4dc
SharonMathewsasi <77880370+SharonMathewsasi@users.noreply.github.com>
2021-05-02 12:38:54 +0530

    Add files via upload

---
+++ Angular/typescriptHandson/Employee-test.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var e1 = {
+    id: 1,
+    name: "A T",
+    salary: 70000,
+    permenant: true
+};
+var d1 = {
+    id: 1,
+    dname: 'Software'
+};
+console.log("ID: " + e1.id);
+console.log("Name: " + e1.name);
+console.log("Salary:" + e1.salary);
+console.log("Permenant:" + e1.permenant);
+console.log("Department ID: " + d1.id);
+console.log("Department Name: " + d1.dname);
---
+++ Angular/typescriptHandson/Employee-test.ts
@@ -0,0 +1,22 @@
+
+import {Employee} from './Employee';
+import {Department} from './Department';
+var e1 :Employee=
+{
+    id:1,
+    name:"A T",
+    salary:70000,
+    permenant:true
+}
+var d1:Department={
+id:1,
+dname:'Software'
+}
+
+console.log(`ID: ${e1.id}`);
+console.log(`Name: ${e1.name}`);
+console.log(`Salary:${e1.salary}`);
+console.log(`Permenant:${e1.permenant}`);
+
+console.log(`Department ID: ${d1.id}`);
+console.log(`Department Name: ${d1.dname}`);
