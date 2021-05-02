4857efcb381c0fcaf0ac04b99aa85412e56c8556
SharonMathewsasi <77880370+SharonMathewsasi@users.noreply.github.com>
2021-05-02 16:25:10 +0530

    Add files via upload

---
+++ Angular/T05-HOL_003/src/app/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponentComponent } from './template-form-component/template-form-component.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
